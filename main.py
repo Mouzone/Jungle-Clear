cbd4e5894259d3547368fe754fc5d6fc443fb2d9
Mouzone <sunnyliu010@gmail.com>
2023-11-18 12:58:00 -0800

    Functions for getting resource and leveling up camps

--- main.py
+++ main.py
@@ -1,57 +1,93 @@
 
 #each indice in each table represents how much resource is given at that camp's level
-camps = {
+class Camp():
+    camps = {
         "Gromp":{
-            gold_table: [80],
-            xp_table: [120, 123, 129, 144, 156, 174]
+            "gold": [80],
+            "xp": [120, 123, 129, 144, 156, 174]
         },
         "Blue":{
-            gold_table: [90],
-            xp_table: [95, 97.38, 102.13, 114, 123.5, 137.75]
+            "gold": [90],
+            "xp": [95, 97.38, 102.13, 114, 123.5, 137.75]
         },
         "Wolves":{
             "Big": {
-                gold_table: [55],
-                xp_table: [50, 51.25, 53.75, 60, 65, 72.5]
+                "gold": [55],
+                "xp": [50, 51.25, 53.75, 60, 65, 72.5]
             },
             "Small": {
-                gold_table: [13],
-                xp_table: [15, 15.38, 16.13, 18, 19.5, 21.75]
+                "gold": [13],
+                "xp": [15, 15.38, 16.13, 18, 19.5, 21.75]
             }
         },
         "Raptors":{
             "Big": {
-                gold_table: [35],
-                xp_table: [20, 20.5, 21.5, 24, 26, 29]
+                "gold_table": [35],
+                "xp_table": [20, 20.5, 21.5, 24, 26, 29]
             },
             "Small": {
-                gold_table: [8],
-                xp_table: [10, 10.25, 10.75, 12, 13, 14.5]
+                "gold": [8],
+                "xp": [10, 10.25, 10.75, 12, 13, 14.5]
             }
         },
-        "Red Buff":{
-            gold_table: [90],
-            xp_table: [95, 97.38, 102.13, 114, 123.5, 137.75]
+        "Red":{
+            "gold": [90],
+            "xp": [95, 97.38, 102.13, 114, 123.5, 137.75]
         },
         "Krugs":{
             "Big":{
-                gold_table: [15],
-                xp_table: [15, 15.38, 16.13, 18, 19.5, 21.75]
+                "gold": [15],
+                "xp": [15, 15.38, 16.13, 18, 19.5, 21.75]
             },
             "Medium":{
-                gold_table: [10],
-                xp_table: [10, 10.25, 10.75, 12, 13, 14.5]
+                "gold": [10],
+                "xp": [10, 10.25, 10.75, 12, 13, 14.5]
             },
             "Small":{
-                gold_table: [14],
-                xp_table: [10, 10.25, 10.75, 12, 13, 14.5]
+                "gold": [14],
+                "xp": [10, 10.25, 10.75, 12, 13, 14.5]
             }
         },
         "Scuttle":{
-            gold_table: [100, 105, 110, 115, 120, 125, 130, 135, 140, 150, 160, 170, 180, 190, 200, 210, 220],
-            xp_table: [55, 57.75, 60.5, 63.25, 66, 68.75, 71.5, 74.25, 77, 82.5, 88, 93.5, 99, 104.5, 110, 115.5, 121]
+            "gold": [100, 105, 110, 115, 120, 125, 130, 135, 140, 150, 160, 170, 180, 190, 200, 210, 220],
+            "xp": [55, 57.75, 60.5, 63.25, 66, 68.75, 71.5, 74.25, 77, 82.5, 88, 93.5, 99, 104.5, 110, 115.5, 121]
         }
     }
+
+    def get_camp_resource(camp, resource):
+        #creeps_clear is a list [0,2] means 0 big 2 small
+        #big is a boolean
+        #small is a number
+        #big and small is optional, if not specified kills whole camp
+        total = 0
+        if camp == "Gromp":
+            total = camps["Gromp"][resource][0]
+        elif camp == "Blue":
+            total = camps["Blue"][resource][0]
+        elif camp == "Wolves":
+            total = camps["Wolves"]["Big"][resource][0] + 2*camp["Wolves"]["Small"][resource][0]
+        elif camp == "Raptors":
+            total = camps["Raptors"]["Big"][resource][0] + 5*camp["Raptors"]["Small"][resource][0]
+        elif camp == "Red":
+            total = camps["Red"][resource][0]
+        elif camp =="Krugs":
+            total = camps["Krugs"]["Big"][resource][0] + camps["Krugs"]["Medium"][resource][0] + 6 * camps["Krugs"]["Big"][resource][0]
+        else:
+            total = camps["Scuttle"][resource][0]
+        level_up_camps(camp, resource)
+        return gold
+        #might need a special function for krugs, ancient krug on death spawns 4 minis, medium krug on death spawns 2 minis
+
+    def level_up_camps(camp, resource):
+        if camp == "Krugs" or camp == "Raptors" or camp == "Wolves":
+            for creep in camps[camp]:
+                if len(camps[camp][creep][resource]) == 1:
+                    return 
+                camps[camp][creep][resource] = camps[camp][creep][resource][1:]
+        else:
+            if len(camps[camp][resource]) == 1:
+                    return 
+            camps[camp][resource] = camps[camp][resource][1:]
 
 class Champion():
     champion_xp = 0
@@ -84,4 +120,3 @@ class Champion():
         self.increment_champion_gold(camp.get_camp_gold())
         self.increment_exp(camp.get_camp_xp())
 
-    
